8d9eb423968cb913465f96eab9878310eaf43a40
Rakshit Sharma <sharmarakshit27@gmail.com>
2025-04-28 23:59:43 +0530

    added signin link in thankyou component

--- frontend/components/Thank.tsx
+++ frontend/components/Thank.tsx
@@ -1,9 +1,13 @@
+import Link from "next/link"
+
 export default function Thank(){
     return <div className="min-h-screen flex items-center justify-center bg-gray-100 px-4"> 
         <div className="w-full max-w-sm bg-white p-8 rounded-2xl shadow-lg ">
           <div className="text-center">
           <h2 className="text-3xl font-bold text-gray-800">Thank you user... 🚀</h2>
           <p className="text-sm text-gray-500 mt-1">Get started</p>
+          <Link href={"/signin"}>
+          <p className="mt-1 text-blue-500">Sign in</p></Link>
           </div>
        </div>
       </div>
